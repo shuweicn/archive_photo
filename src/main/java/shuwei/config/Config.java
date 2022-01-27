aec8fa2f1b9316284aa4feb73307e5dbd11ac239
zhao.shuwei <zhao.shuwei@immomo.com>
2022-01-27 20:00:12 +0800

    fix bug

--- src/main/java/shuwei/config/Config.java
+++ src/main/java/shuwei/config/Config.java
@@ -5,8 +5,8 @@ import java.util.*;
 
 public class Config {
     public static final Set<String> IGNORE_FILES = new HashSet<String>(Arrays.asList(".DS_Store"));
-    public static final boolean USE_DEVICE_PATH = false;
-    public static final boolean USE_DATE_PATH = false;
+    public static final boolean USE_DEVICE_PATH = true;
+    public static final boolean USE_DATE_PATH = true;
 
 //    public static final String EXISTS_CHECK_FILE = "/Users/momo/Downloads/sha256.chk";
 //    public static final String EXIFTOOLS = "/usr/local/bin/exiftool";
@@ -15,7 +15,7 @@ public class Config {
 
     public static final String EXISTS_CHECK_FILE = "/data/smb/sha256.chk";
     public static String EXIFTOOLS = "/usr/local/Image-ExifTool-12.03/exiftool";
-    public static String PHOTO_PATH = "/data/smb/rescue";
-    public static String DESTINATION_PATH = "/data/smb/chunyan_dest";
+    public static String PHOTO_PATH = "/data/smb/chunyan-single-photo-202201";
+    public static String DESTINATION_PATH = "/data/smb/chunyan-single-photo-202201-dist";
 
 }
